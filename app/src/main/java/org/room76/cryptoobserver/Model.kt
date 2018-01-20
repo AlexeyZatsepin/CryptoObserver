d91508249398004bb8944cc6ab23c0d2f02fd413
Arsenii Zhdanov <arsenzhd@gmail.com>
2018-01-20 19:20:14 +0200

    Add Chart backend

--- app/src/main/java/org/room76/cryptoobserver/Model.kt
+++ app/src/main/java/org/room76/cryptoobserver/Model.kt
@@ -1,17 +1,36 @@
 package org.room76.cryptoobserver
 
 import com.google.gson.annotations.SerializedName
-
-/**
- * Created by Alexey on 1/19/18.
- */
+import java.sql.Date
+import java.time.LocalDateTime
+import java.util.*
 
 object Model {
-    data class Currency(@SerializedName("Currency") var currency:String,
-                        @SerializedName("CurrencyLong") var currencyLong:String,
+    data class Currency(@SerializedName("Currency") var currency: String,
+                        @SerializedName("CurrencyLong") var currencyLong: String,
                         @SerializedName("MinConfirmation") var minConfirmation: Int,
                         @SerializedName("TxFee") var txFee: Float,
                         @SerializedName("IsActive") var isActive: Boolean,
                         @SerializedName("CoinType") var coinType: String,
                         @SerializedName("BaseAddress") var baseAddress: String)
+
+    data class OhlcKraken(
+            @SerializedName("error") var error: List<String>,
+            @SerializedName("result") var body: OhlcResultEthUsd)
+
+    data class OhlcResultEthUsd(
+            @SerializedName("XETHZUSD") var pair: List<List<String>>,
+            @SerializedName("last") var lastTransaction: Long
+    )
+
+    data class Chart(
+            var exchangerName: String,
+            var pair: String,
+            var chartData: List<ChartData>
+    )
+
+    data class ChartData(
+           var date: Calendar,
+           var average: Long
+    )
 }
