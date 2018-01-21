fe5ed330a1e01e439c1b9798a10f2aece280a765
Alexey Zatsepin <alexzatsepin7@gmail.com>
2018-01-21 16:19:23 +0200

    add spinners

--- app/src/main/java/org/room76/cryptoobserver/ChartFragment.kt
+++ app/src/main/java/org/room76/cryptoobserver/ChartFragment.kt
@@ -9,14 +9,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.BounceInterpolator
+import android.widget.*
 import com.db.chart.animation.Animation
 import com.db.chart.model.LineSet
 import com.db.chart.renderer.AxisRenderer
 import com.db.chart.tooltip.Tooltip
 import com.db.chart.util.Tools
 import com.db.chart.view.LineChartView
-import android.widget.ImageButton
-import android.widget.Toast
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -30,16 +29,73 @@ class ChartFragment : Fragment() {
     private lateinit var mChart: LineChartView
     private lateinit var mTip: Tooltip
     private lateinit var mUpdateBtn: ImageButton
+    private lateinit var mCurrencySelector: Spinner
+    private lateinit var mIntervalSelector: Spinner
+    private lateinit var mFrom: TextView
+    private lateinit var mTo: TextView
     private val TAG = ChartFragment::class.java.name
+
+    private val currencies = arrayOf("ETH/USD", "DASH/USD", "BCH/USD", "XRP/USD", "LTC/USD")
+    private val intervals = arrayOf("1", "5", "15", "30", "60", "240", "1440", "10080", "21600") // minutes
+
+    private val start : Calendar = Calendar.getInstance()
+    private val end : Calendar = Calendar.getInstance()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_chart,container)
+
+        start.time = Date(System.currentTimeMillis())
+        start.set(start.time.year,start.time.month,start.time.day-7)
+        end.time = Date(System.currentTimeMillis())
+
         mChart = root.findViewById(R.id.chart)
         mUpdateBtn = root.findViewById(R.id.update)
         mUpdateBtn.setOnClickListener {
-            updateChart()
+//            populateChart(currencies[mCurrencySelector.selectedItemPosition]
+//                    ,intervals[mIntervalSelector.selectedItemPosition])
         }
-        populateChart()
+        mFrom = root.findViewById(R.id.from)
+        mTo = root.findViewById(R.id.to)
+        mFrom.text = Utils.dateFormatWithYear.format(start.time)
+        mTo.text = Utils.dateFormatWithYear.format(end.time)
+
+        populateChart(currencies[0],intervals[6])
+
+        val adapter = ArrayAdapter<String>(context, R.layout.spinner_item, currencies)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        mCurrencySelector = root.findViewById(R.id.currency)
+        mCurrencySelector.adapter = adapter
+        mCurrencySelector.setSelection(0)
+        mCurrencySelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View,
+                                        position: Int, id: Long) {
+                if (mIntervalSelector.selectedItemPosition > 0){
+//                    dismiss()
+//                    updateChart(currencies[position],intervals[mIntervalSelector.selectedItemPosition])
+                }
+            }
+
+            override fun onNothingSelected(arg0: AdapterView<*>) {}
+        }
+
+        val adapterEx = ArrayAdapter(context, R.layout.spinner_item, intervals)
+        adapterEx.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        mIntervalSelector = root.findViewById(R.id.interval)
+        mIntervalSelector.adapter = adapterEx
+        mIntervalSelector.setSelection(6)
+        mIntervalSelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View,
+                                        position: Int, id: Long) {
+                if (mCurrencySelector.selectedItemPosition > 0){
+//                    dismiss()
+//                    updateChart(currencies[mCurrencySelector.selectedItemPosition],intervals[position])
+                }
+            }
+
+            override fun onNothingSelected(arg0: AdapterView<*>) {}
+        }
         return root
     }
 
@@ -48,16 +104,18 @@ class ChartFragment : Fragment() {
         dismiss()
     }
 
-    private fun populateChart(){
-        requestChartData("ETHUSD", "1440", object : Utils.Action<Model.Chart> {
+    private fun populateChart(pair: String, interval:String){
+        requestChartData(pair.replace("/",""), interval, object : Utils.Action<Model.Chart> {
             override fun action(data: Model.Chart) {
                 val labels: MutableList<String> = mutableListOf()
                 val values: MutableList<Float> = mutableListOf()
-                data.chartData.forEach { it->
-                    run {
-                        labels.add(it.date.get(Calendar.DATE).toString())
-                        values.add(it.average)
-                    }
+                data.chartData.filter { it.date.timeInMillis > start.timeInMillis }
+                        .filter { it.date.timeInMillis < end.timeInMillis }
+                        .forEach { it->
+                            run {
+                                labels.add(Utils.dateFormat.format(it.date.time))
+                                values.add(it.average)
+                            }
                 }
                 show(labels.toTypedArray(), values.toFloatArray())
             }
@@ -65,11 +123,12 @@ class ChartFragment : Fragment() {
     }
 
     private fun show(labels: Array<String>, values: FloatArray ) {
+        Log.i(TAG,"showing " + values.size + "elements")
         // Tooltip
         mTip = Tooltip(context, R.layout.tooltip, R.id.value)
 
         mTip.setVerticalAlignment(Tooltip.Alignment.BOTTOM_TOP)
-        mTip.setDimensions(Tools.fromDpToPx(70f).toInt(), Tools.fromDpToPx(25f).toInt())
+        mTip.setDimensions(Tools.fromDpToPx(80f).toInt(), Tools.fromDpToPx(25f).toInt())
 
         mTip.setEnterAnimation(PropertyValuesHolder.ofFloat(View.ALPHA, 1f),
                 PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f),
@@ -114,11 +173,13 @@ class ChartFragment : Fragment() {
                         .withEndAction(chartAction))
     }
 
-    private fun updateChart() {
-        requestChartData("ETHUSD", "1440", object : Utils.Action<Model.Chart> {
+    private fun updateChart(pair: String, interval: String) {
+        requestChartData(pair.replace("/",""), interval, object : Utils.Action<Model.Chart> {
             override fun action(data: Model.Chart) {
                 val values: MutableList<Float> = mutableListOf()
-                data.chartData.forEach { it -> values.add(it.average) }
+                data.chartData.filter { it.date.timeInMillis > start.timeInMillis }
+                        .filter { it.date.timeInMillis < end.timeInMillis }
+                        .forEach { it -> values.add(it.average) }
                 mChart.dismissAllTooltips()
                 mChart.updateValues(0, values.toFloatArray())
                 mChart.updateValues(1, values.toFloatArray())
@@ -130,9 +191,12 @@ class ChartFragment : Fragment() {
 
 
     private fun dismiss() {
-        mChart.dismissAllTooltips()
-        mChart.dismiss(Animation().setInterpolator(BounceInterpolator())
-                .withEndAction({Log.v(TAG,"animation dissmised")}))
+        if (mChart.childCount > 0) {
+            mChart.dismissAllTooltips()
+            mChart.dismiss(Animation().setInterpolator(BounceInterpolator())
+                    .withEndAction({Log.v(TAG,"animation dissmised")}))
+            mChart.removeAllViewsInLayout()
+        }
     }
 
     private fun requestChartData(pair: String, interval:String, action: Utils.Action<Model.Chart>) {
@@ -147,9 +211,13 @@ class ChartFragment : Fragment() {
             override fun onResponse(call: Call<Model.OhlcKraken>,
                                     response: Response<Model.OhlcKraken>) {
                 if (response.isSuccessful){
-                    val chart = Utils.ohlcKrakenToChart(response.body())
-                    Log.i(TAG, "Response objects: " + chart.chartData.size)
-                    action.action(chart)
+                    val chart: Model.Chart = Utils.ohlcKrakenToChart(response.body())
+                    if (chart.chartData.isEmpty()) {
+                        Toast.makeText(context,"No data",Toast.LENGTH_LONG).show()
+                    }else {
+                        Log.i(TAG, "Response objects: " + chart.chartData.size)
+                        action.action(chart)
+                    }
                 } else {
                     Log.e(TAG, response.errorBody().string())
                     Toast.makeText(context,"Network error",Toast.LENGTH_LONG).show()
--- app/src/main/java/org/room76/cryptoobserver/Utils.kt
+++ app/src/main/java/org/room76/cryptoobserver/Utils.kt
@@ -2,22 +2,30 @@ package org.room76.cryptoobserver
 
 import java.sql.Date
 import java.sql.Timestamp
+import java.text.SimpleDateFormat
 import java.util.*
 
 
 object Utils {
+
+    val dateFormatWithYear = SimpleDateFormat("HH:mm dd/MM/yyyy")
+    val dateFormatWithMonth = SimpleDateFormat("HH:mm dd/MM")
+    val dateFormatWithDay = SimpleDateFormat("HH:mm dd")
+    val dateFormat = SimpleDateFormat("HH:mm")
 
     interface Action<in T> {
         fun action(data:T)
     }
 
     fun ohlcKrakenToChart(values: Model.OhlcKraken): Model.Chart {
-        val chartData = ArrayList<Model.ChartData>()
-        for (p in values.body.pair) {
-            val average = (p[2].toString().toFloat()+ p[3].toString().toFloat()) / 2
-            val date = Calendar.getInstance()
-            date.time = Date(Timestamp((p[0] as Double).toLong()).time)
-            chartData.add(Model.ChartData(date, average))
+        val chartData: MutableList<Model.ChartData> = mutableListOf()
+        if (values.error.isEmpty()) {
+            for (p in values.body.pair) {
+                val average = (p[2].toString().toFloat()+ p[3].toString().toFloat()) / 2
+                val date = Calendar.getInstance()
+                date.time = Date(Timestamp((p[0] as Double).toLong()).time)
+                chartData.add(Model.ChartData(date, average))
+            }
         }
         return Model.Chart("Kraken", "ETH/USD", chartData)
     }
