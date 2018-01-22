4173370ef629956a60f3d4405f0f950a08025552
Arsenii Zhdanov <arsenzhd@gmail.com>
2018-01-22 19:44:03 +0200

    finist up to point

--- app/src/main/java/org/room76/cryptoobserver/Model.kt
+++ app/src/main/java/org/room76/cryptoobserver/Model.kt
@@ -1,18 +1,20 @@
 package org.room76.cryptoobserver
 
 import com.google.gson.annotations.SerializedName
-import java.sql.Date
-import java.time.LocalDateTime
+import retrofit2.Callback
+import java.lang.IllegalArgumentException
 import java.util.*
 
 object Model {
-    data class Currency(@SerializedName("Currency") var currency: String,
-                        @SerializedName("CurrencyLong") var currencyLong: String,
-                        @SerializedName("MinConfirmation") var minConfirmation: Int,
-                        @SerializedName("TxFee") var txFee: Float,
-                        @SerializedName("IsActive") var isActive: Boolean,
-                        @SerializedName("CoinType") var coinType: String,
-                        @SerializedName("BaseAddress") var baseAddress: String)
+    data class BittrixMarketSummary(
+            @SerializedName("success") var success: Boolean,
+            @SerializedName("message") var message: String,
+            @SerializedName("result") var result: List<BittrixMarketResult>
+    )
+
+    data class BittrixMarketResult(
+            @SerializedName("Last") var last: Double
+    )
 
     data class OhlcKraken(
             @SerializedName("error") var error: List<String>,
@@ -30,7 +32,100 @@ object Model {
     )
 
     data class ChartData(
-           var date: Calendar,
-           var average: Float
+            var date: Calendar,
+            var average: Float
     )
+
+    interface Currency {
+        fun getCurrency(): Double
+    }
+
+    class BittrixCurrency(result: BittrixMarketResult) : Currency {
+        var result: BittrixMarketResult = result
+        override fun getCurrency(): Double {
+            return this.result.last
+        }
+
+    }
+
+    interface Market {
+        fun getName(): String
+        fun getToFee(): Double
+        fun getFromFee(): Double
+        fun updateCurrencyCall(coin: String, call: Callback<Currency>): Double
+    }
+
+
+    class YobitMarket : Market {
+        override fun getName(): String {
+            return "Yobit"
+        }
+
+        override fun getToFee(): Double {
+            return 0.2
+        }
+
+        override fun getFromFee(): Double {
+            return 0.2
+        }
+
+        override fun updateCurrencyCall(coin: String, call: Callback<Currency>): Double {
+            return 0.0
+        }
+    }
+
+    class KrakenMarket : Market {
+
+        override fun getName(): String {
+            return "Kraken"
+        }
+
+        override fun getToFee(): Double {
+            return 0.1
+        }
+
+        override fun getFromFee(): Double {
+            return 0.0
+        }
+
+        override fun updateCurrencyCall(coin: String, call: Callback<Currency>): Double {
+            return 0.0
+        }
+    }
+
+    class BittrexMarket : Market {
+        override fun getName(): String {
+            return "Bittrex"
+        }
+
+        override fun getToFee(): Double {
+            return 0.25
+        }
+
+        override fun getFromFee(): Double {
+            return 0.25
+        }
+
+        override fun updateCurrencyCall(coin: String,
+                                        call: Callback<Currency>): Double {
+
+            return 0.0
+        }
+    }
+
+    class MarketFactory() {
+
+        fun getMarketForName(name: String): Market {
+            if (name.equals("Bittrix")) {
+                return BittrexMarket()
+            }
+            if (name.equals("Yobit")) {
+                return YobitMarket()
+            }
+            if (name.equals("Kraken")) {
+                return KrakenMarket()
+            }
+            throw IllegalArgumentException("No market with name " + name)
+        }
+    }
 }
--- app/src/main/java/org/room76/cryptoobserver/StatsFragment.kt
+++ app/src/main/java/org/room76/cryptoobserver/StatsFragment.kt
@@ -7,12 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
+import java.util.concurrent.ArrayBlockingQueue
+import kotlin.collections.ArrayList
 
-
-/**
- * Created by Alexey on 1/21/18.
- */
-class StatsFragment  : Fragment() {
+class StatsFragment : Fragment() {
 
     private lateinit var mCurrencySelector: Spinner
     private lateinit var mExchangeSelector: Spinner
@@ -22,8 +24,11 @@ class StatsFragment  : Fragment() {
     val currencies = arrayOf("ETH", "BTC", "DASH", "RPL")
     val exchanges = arrayOf("Kraken", "Yobit", "Bittrix")
 
+    private var lock = Any()
+    private var responeses = ArrayList<MarketCurrency>()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val root = inflater.inflate(R.layout.fragment_stats,container)
+        val root = inflater.inflate(R.layout.fragment_stats, container)
         val adapter = ArrayAdapter<String>(context, R.layout.spinner_item, currencies)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
@@ -56,11 +61,77 @@ class StatsFragment  : Fragment() {
         mTextView = root.findViewById(R.id.result)
 
         mButton.setOnClickListener({
-            Toast.makeText(context, "Calculate result and set it to result view", Toast.LENGTH_SHORT).show()
-            mTextView.text = "Result"
+            Toast.makeText(context, "Calculate result and set it to result view",
+                    Toast.LENGTH_SHORT).show()
+            responeses = ArrayList()
+            mTextView.text = "Result: "
+            val selectedMarket = Model.MarketFactory()
+                    .getMarketForName(mExchangeSelector.selectedItem.toString())
+            val coin = mCurrencySelector.selectedItem.toString()
+            for (ex in exchanges) {
+                var market = Model.MarketFactory().getMarketForName(ex)
+                market.updateCurrencyCall(coin, object : Callback<Model.Currency> {
+                    override fun onFailure(call: Call<Model.Currency>?, t: Throwable?) {
+                    }
+
+                    override fun onResponse(call: Call<Model.Currency>?, response: Response<Model.Currency>?) {
+                        val currency = response!!.body().getCurrency()
+                        if (market.getName().equals(selectedMarket.getName())) {
+                            updateView(MarketCurrency(market, currency, true))
+                        } else {
+                            updateView(MarketCurrency(market, currency, false))
+                        }
+                    }
+                })
+            }
         })
+
 
         return root
     }
 
+    fun updateView(marketCurrency: MarketCurrency) {
+        synchronized(lock) {
+            responeses.add(marketCurrency)
+            if (responeses.size == exchanges.size - 1) {
+                for (fromMarket in responeses) {
+                    if (fromMarket.from) {
+                        for (market in responeses) {
+                            if (!market.equals(fromMarket)) {
+                                var profit = isProfit(fromMarket, market)
+                                updateText(profit, fromMarket, market)
+                            }
+                        }
+                        break
+                    }
+                }
+
+            }
+        }
+    }
+
+    fun isProfit(fromMarket: MarketCurrency, market: MarketCurrency): Boolean {
+        var profit = (fromMarket.currency - fromMarket.currency * fromMarket.market.getFromFee())
+        profit = profit / market.currency - market.market.getToFee() * profit
+        return profit > 1
+    }
+
+    fun updateText(profit: Boolean, fromMarket: MarketCurrency, market: MarketCurrency) {
+        if (profit) {
+            val s = String
+                    .format("You will make profit transferring %s from exchanger %s to exchanger %s",
+                            mCurrencySelector.selectedItem.toString(),
+                            fromMarket.market.getName(), market.market.getName())
+            mTextView.text = mTextView.text.toString() + s
+        } else {
+            val s = String
+                    .format("You will make no profit transferring %s from exchanger %s to exchanger %s",
+                            mCurrencySelector.selectedItem.toString(),
+                            fromMarket.market.getName(), market.market.getName())
+            mTextView.text = mTextView.text.toString() + s
+        }
+    }
+
+    class MarketCurrency(var market: Model.Market, var currency: Double, var from: Boolean) {
+    }
 }
